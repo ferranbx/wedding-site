b4ece5410c53014837e91a430e757be7b9b32ef6
Ferran Blanché <ferran.bx@gmail.com>
2020-05-01 18:45:07 +0200

    Page refinement

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="home" class="page">
+    <div id="main" class="section">
+      <div class="section-header">
+        <p class="text text-font-accent text-size-large">{{ $t("logo") }}</p>
+      </div>
+      <div class="section-content">
+        <h1 class="text text-font-base text-size-largest">{{ $t("title") }}</h1>
+        <h2 class="text text-font-accent text-size-larger text-color-accent-base">{{ $t("subtitle") }}</h2>
+        <p class="text text-font-base text-size-base">{{ $t("letter-1") }}</p>
+        <p class="text text-font-base text-size-base">{{ $t("letter-2") }}</p>
+        <p class="text text-font-base text-size-base">{{ $t("letter-3") }}</p>
+        <p class="text text-font-base text-size-base">{{ $t("letter-4") }}</p>
+      </div>
+      <div id="action">
+                <div class="btn">
+                    <a href="https://covid19responsefund.org/" target="blank">{{ $t("donate") }}</a>
+                    <i class="fas fa-arrow-right btn-icon-right"></i>
+                </div>
+            </div>
+      <div class="section-footer">
+        <LocaleChanger />
+      </div>
+    </div>
+    <div id="side" class="section"></div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import LocaleChanger from "@/components/LocaleChanger.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    LocaleChanger
   }
 };
 </script>
